74aeeb4bb83458f93470134d1339d02fab879f24
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r20s6.42lisboa.com>
2024-04-10 15:51:59 +0100

    2

---
+++ first/index.js
@@ -0,0 +1,26 @@
+
+let name = 'Sukuna';
+console.log(name);
+
+let vessel = {
+    name: 'Yuuji',
+    age: 16
+};
+
+console.log(vessel);
+
+vessel.name = 'Megumi';
+
+console.log(vessel);
+
+vessel['name'] = 'Sukuna';
+
+console.log(vessel.name);
+
+let SukunaVictims = ['Jogo', 'Marohaga'];
+
+console.log(SukunaVictims);
+
+SukunaVictims[2] = 'Gojo';
+
+console.log(SukunaVictims);
