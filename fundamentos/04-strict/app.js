cfadecb30be96224b5c0979d7d2d56f14e92efff
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r20s6.42lisboa.com>
2024-06-10 17:15:17 +0100

    14

---
+++ fundamentos/04-strict/app.js
@@ -0,0 +1,4 @@
+"use strict" //uses the updated format of java-script
+
+let Name = "John"; //with the updated version we have to declare our variables
+console.log(Name);
---
+++ fundamentos/06-variables-ex/ex02.js
@@ -0,0 +1,8 @@
+let A = 10, B = 20, C = A;
+
+console.clear();
+console.log("C = ", C);
+A = B;
+B = C;
+console.log("A = ", A);
+console.log("B = ", B);
